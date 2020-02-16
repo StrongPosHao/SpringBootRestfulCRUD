2788a18a589591597d309d7ac876ee9a7e24756e
StrongPosHao <lantian475587063@qq.com>
2020-02-16 20:16:08 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Spring Boot Learning Project 2
+* CRUD Project
+* Restful Style
---
+++ src/main/java/com/example/springboot/config/MyMvcConfig.java
@@ -0,0 +1,48 @@
+package com.example.springboot.config;
+
+import com.example.springboot.component.LoginHandlerInterceptor;
+import com.example.springboot.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 浏览器发送 /atguigu 请求来到success
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+
+    // 所有的WebMvcConfigurer组件都会一起起作用
+    @Bean // 注册进Spring容器使其生效
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            // 注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                // 静态资源: *.css, *.js
+                // SpringBoot已经做好了静态资源映射
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html", "/", "/user/login", "/webjars/**", "/asserts/**");
+            }
+        };
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
+    }
+}
